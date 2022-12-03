9c343bab540107ac706b0a21d3fbfb0aa4a68092
Ali.Barzegari.Dahaj <ali.barzegari.dahaj.1400@gmail.com>
2022-12-03 19:48:30 +0330

    aval

---
+++ ali.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#define pi = 3.141592
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    cout << "hi ali";
+
+    return 0;
+}
