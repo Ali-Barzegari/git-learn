a758299be20476c357975d5b29ca89122faa8594
Ali.Barzegari.Dahaj <ali.barzegari.dahaj.1400@gmail.com>
2022-12-10 21:52:56 +0330

    tamrin 2(taghir 2)

--- amir.cpp
+++ amir.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <stdio.h>
-#include <cmath>
-#include <string>
+
 #include <conio.h>
 #define pi = 3.141592
 using namespace std;
 int main()
 {
     ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    int n, m;
     cout << "tamrin 2";
     cout << "tamrin git ";
     cout << "taghir";
